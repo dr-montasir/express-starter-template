47b17919b6cecf214c3e0d634012b5c6d95b99fc
montasir <mutawirsoft@mail.ru>
2020-03-15 01:07:10 +0300

    Overwrite defaultLayout by the admin route

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// Overwrite defaultLayout by the admin route
+router.all('/*', (req, res, next) => {
+	req.app.locals.layout = 'admin';
+	next();
+});
+
 // The Admin  Dashboard (Admin  Index) Route
 router.get('/', (req, res) => {
 	res.render('admin/dashboard');
